2bb68a8e718889462fc0fb40719ebca50162f86e
JGSMN <lee49000eh@naver.com>
2024-07-31 17:50:57 +0900

    0731 17:50 commit

--- c_javascript.js/c_dataType.js/c_dataType.js
+++ c_javascript.js/c_dataType.js/c_dataType.js
@@ -54,3 +54,47 @@ console.log(typeof bool1); // boolean
 
 console.log(bool2); // false
 console.log(typeof bool2); // boolean
+
+
+//! 4. null & undefined 자료형
+
+// 4-1 null: 아무것도없다.
+// >> 변수의 데이터가 유효하지 않은 경우
+
+// 4-2 undefined: 정의되지 않았다.
+// >> 변수가 선언되었으나, 아직 어떠한 값도 할당되지 않았을 때의 기본값
+
+let hello; // 변수 선언
+
+// : clg: console.log()의 코드 스니팻(축약 단축키)
+console.log(hello);
+
+
+
+let num1 = 123;
+let num2 = 456;
+
+console.log(num1 + num2); // 579
+
+// 123456
+let stringNum1 = String(num1);
+let stringNum2 = String(num2);
+console.log(stringNum1 + stringNum2); 123456
+
+// 2. Number() 함수
+
+let notANumber = '안녕하세요'; // 문자열
+console.log(Number(notANumber)); // NaN: Not a Number - 숫자로 변환할 수 없는 값을 변환 시도할 경우
+
+console.log(Number(stringNum1)); // 123
+console.log(typeof Number(stringNum1)); // number
+
+// 3. Boolean() 함수
+
+// false: false, 0, '', null, undefined, NaN
+// true: 위의 값을 제외한 모든 데이터를 true로 간주
+
+console.log(Boolean('hello')); // true
+console.log(Boolean('')); // false
+
+console.log(Boolean(123)); //
